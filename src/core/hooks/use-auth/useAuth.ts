71b1272ef24fc36996daf357b833d55dd1b45db4
Denlich <muchalovych@gmail.com>
2024-07-23 12:43:37 +0200

    build AuthContext

---
+++ src/core/hooks/use-auth/useAuth.ts
@@ -0,0 +1,14 @@
+import { UseAuthReturn } from './types';
+import { useAuthContext } from './use-auth-context';
+
+export const useAuth = (): UseAuthReturn => {
+  const { user, update } = useAuthContext();
+
+  const isLoggedIn = !!user;
+
+  return {
+    isLoggedIn,
+    user,
+    update,
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import theme from '@/core/theme';
 import { router } from '@/core/router';
+import { AuthContext } from '@/core/hooks/use-auth/use-auth-context';
 
 import '@/core/theme/styles';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <AuthContext>
+          <RouterProvider router={router} />
+        </AuthContext>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
